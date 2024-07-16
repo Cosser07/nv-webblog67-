e1e4232908acffaad68792a49871ab40c9484b0e
WHmild04 <gmpwh04@gmail.com>
2024-07-16 09:57:09 +0700

    add user

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,10 @@
 let express = require('express');
+let bodyParser = require('body-parser')
+
 const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/status', function(req, res){
     console.log('------------------------------');
@@ -17,4 +22,26 @@ let port = 8081
 
 app.listen(port, function(){
     console.log('Server running on http://localhost:' + port)
-});
+})
+app.get('/user/:userId',function (req , res) {
+    res.send('เรียกดูข้อมูล ผู้ใช้งานทั้งหมด '+ req.params.userId)
+    
+})
+app.get('/users',function (req , res) {
+    res.send('เรียกดูข้อมูล ผู้ใช้งานทั้งหมด ')
+       
+})
+
+app.post('user/', function(req,res){
+    res.send('ทำการสร้างผู้ใช้งาน'+JSON.stringify(req.body))
+
+})
+
+app.put('user/:userId', function(req,res){
+    res.send('ทำการแก้ไขผู้ใช้งาน'+req.params.userId + ' : '+JSON.stringify(req.body))
+})
+
+app.delete('user/:userId', function (req , res) {
+    res.send('ทำการลบผู้ใช้งาน' + req.params.userId + ' : ' +JSON.stringify(req.body))
+})
+
